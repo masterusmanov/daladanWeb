e025ca1905140a5f63a392bc211d2ad39d48f063
Elyor Usmanov <94918957+masterusmanov@users.noreply.github.com>
2024-01-21 15:20:34 +0500

    DaladanWeb

---
+++ src/pages/Homepage/Homepage.jsx
@@ -0,0 +1,11 @@
+// import React from "react";
+
+function Homepage(){
+    return(
+        <div className="container mx-auto">
+            <h1 className="text-red-500 font-[900]">salom</h1>
+        </div>
+    )
+}
+
+export default Homepage;
